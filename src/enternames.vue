95ac12508eaab73c9228b0faf3e5dc22058e5360
jacintogomez <63368475+jacintogomez@users.noreply.github.com>
2024-12-03 21:32:04 -0500

    update enternames with options

--- src/enternames.vue
+++ src/enternames.vue
@@ -1,38 +1,29 @@
-<template>
-  <div>
-    <h1>Enter Names</h1>
-    <form @submit.prevent="startGame">
-      <div v-for="(name, index) in names" :key="index">
-        <input v-model="names[index]" placeholder="Enter name" />
-        <button @click.prevent="removeName(index)">Remove</button>
-      </div>
-      <button type="button" @click="addName">Add Name</button>
-      <button type="submit">Start Game</button>
-    </form>
-  </div>
-</template>
+<script setup>
+import {ref} from 'vue';
+import {useRouter} from 'vue-router';
+import {characters} from './store.js';
 
-<script>
-export default {
-  data() {
-    return {
-      names: [''] // Start with an empty array for user names
-    };
-  },
-  methods: {
-    addName() {
-      this.names.push('');
-    },
-    removeName(index) {
-      this.names.splice(index, 1);
-    },
-    startGame() {
-      // Navigate to the roulette page and pass data
-      this.$router.push({
-        path: '/roulette',
-        query: { names: JSON.stringify(this.names) }
-      });
-    }
-  }
-};
+const router=useRouter();
+const names=ref(['']);
+
+const addname=()=>{names.value.push('');}
+const removename=(index)=>{names.value.splice(index,1);}
+const startgame=()=>{
+    characters.value=names.value.filter(name=>name.trim()!=='');
+    router.push('/roulette');
+}
 </script>
+
+<template>
+    <div>
+        <h1>Enter Character Names</h1>
+        <form @submit.prevent="startgame">
+            <div v-for="(name,index) in names" :key="index">
+                <input v-model="names[index]" placeholder="Enter name">
+                <button @click.prevent="removename(index)">-</button>
+            </div>
+            <button type='button' @click="addname">+</button>
+            <button type='submit'>Begin</button>
+        </form>
+    </div>
+</template>
